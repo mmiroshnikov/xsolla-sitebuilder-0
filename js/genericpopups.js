6ad7a00b3ecab7926b16d3103113fefb1bf7917a
Mike Miroshnikov <m.miroshnikov@xsolla.com>
2019-01-07 17:31:22 -0800

    Z

--- js/genericpopups.js
+++ js/genericpopups.js
@@ -6,9 +6,23 @@ define([
   function GenericPopups() {
 
     this.popups = this.getPopups();
+    this.getZs = this.getZs();
 
   }
 
+
+    GenericPopups.prototype.getZs = function () {
+      $('[data-xpop-z=\'' + onePopName + '\']').on({
+        click: function (evt) {
+          var popData = oneTrigger.dataset.xpopZ;
+          p[onePopUniqName]['$popB'].removeClass('shown');
+          p[onePopUniqName]['$popZ'].removeClass('shown');
+          setTimeout(function () {
+            p[onePopUniqName]['$pop'].removeClass('shown');
+          }, 200)
+        }
+      });
+    }
 
   GenericPopups.prototype.getPopups = function () {
     var p = {};
@@ -20,7 +34,8 @@ define([
         var toggl = false;
         if (popData.split(',')[2]) toggl = true;
         var trigger = oneTrigger;
-        p[popName + Object.keys(p).length] = {
+        p[popName] =  p[popName] || {}
+        p[popName][popName + Object.keys(p).length] = {
           'name': popName,
           'trigger': trigger,
           '$pop': '',
@@ -31,7 +46,8 @@ define([
     });
 
 
-    Object.keys(p).forEach(function (onePopUniqName, i) {
+    Object.keys(p).forEach(function (popName, i) {
+      Object.keys(p[popName]).forEach(function (onePopUniqName, i) {
         var onePopName = p[onePopUniqName]['name'];
         p[onePopUniqName]['$pop'] = $('[data-xpop=\'' + onePopName + '\']');
         p[onePopUniqName]['$popB'] = p[onePopUniqName]['$pop'].find('[data-xpop=\'b\']');
@@ -65,17 +81,11 @@ define([
           }, 200)
         }
       });
-      $('[data-xpop-z=\'' + onePopName + '\']').on({
-        click: function (evt) {
-          p[onePopUniqName]['$popB'].removeClass('shown');
-          p[onePopUniqName]['$popZ'].removeClass('shown');
-          setTimeout(function () {
-            p[onePopUniqName]['$pop'].removeClass('shown');
-          }, 200)
-        }
-      });
 
     })
+    });
+
+
     return p;
   }
 
